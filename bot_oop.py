ccb558e014a9f3160a528ea2ea2db3f4480deb0a
Vel-Vladimir <133573042+Vel-Vladimir@users.noreply.github.com>
2023-07-21 20:28:30 +0300

    Update bot_oop.py

--- bot_oop.py
+++ bot_oop.py
@@ -1,55 +1,48 @@
 from collections import UserDict
 
-class Field:
-    pass
 
-
-class Phone(Field):
+class Phone:
     def __init__(self, phone_number: str):
         self.value = phone_number
+    
 
-
-class Name(Field):
+class Name:
     def __init__(self, name: str):
         self.value = name
 
 
-class Record(Field):
+class Record:
     def __init__(self, name: Name, *phones: Phone):
-        self.phones = list(phones)
+        self.phones = list(set(phones))
         self.name = name
 
-    def add_phone(self, phone: Phone) -> None:
-        self.phones.append(phone)
+    def add_phone(self, phone_to_add: Phone) -> None:
+        is_exist = False
+        for exist_phone in self.phones:
+            if phone_to_add.value == exist_phone.value:
+                is_exist = True
+        if not is_exist:
+            self.phones.append(phone_to_add)
+
 
     def del_phone(self, phone: Phone) -> None:
         try:
             self.phones.remove(phone)
 
         except ValueError:
-            print("You try to delete number that is absent in the list!")
+            message = f"You try to delete number {phone.value} that is absent in the list!"
 
     def edit_phone(self, current_phone: Phone, new_phone: Phone) -> None:
         try:
             index = self.phones.index(current_phone)
             self.phones[index] = new_phone
         except ValueError:
-            print("You try to edit number that is absent in the list!")
-
-    def show_phones(self) -> None:
-        header = "|     The phone list     |"
-        print("-" * len(header))
-        print(header)
-        print("-" * len(header))
-        for phone in self.phones:
-            print(f'|{phone.value:^24}|')
-        print("-" * len(header))
+            message = f"You try to edit number {current_phone.value} that is absent in the list!"
 
 
 class AddressBook(UserDict):
     def add_record(self, record: Record) -> None:
         self.data[record.name.value] = record
-
 
 if __name__ == '__main__':
     name = Name('Bill')
